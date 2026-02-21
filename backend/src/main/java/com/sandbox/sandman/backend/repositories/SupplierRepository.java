22e68c7ce3b04582ddaad2c61005853b45431bb9
Nutchanon Charoensermsiri <t445377@scbcorp.co.th>
2026-02-22 00:21:46 +0700

    add new query repo

--- backend/src/main/java/com/sandbox/sandman/backend/repositories/SupplierRepository.java
+++ backend/src/main/java/com/sandbox/sandman/backend/repositories/SupplierRepository.java
@@ -1,7 +1,22 @@
 package com.sandbox.sandman.backend.repositories;
 
+import com.sandbox.sandman.backend.model.dto.SupplierOrderDTO;
 import com.sandbox.sandman.backend.model.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+    // สังเกตว่าไม่มี nativeQuery = true แล้ว
+    @Query(value="""
+        SELECT new com.sandbox.sandman.backend.model.dto.SupplierOrderDTO(
+            o.orderId, s.supplierName, s.contactPerson, 
+            o.orderDate, o.deliveryDate, o.status, o.notes
+        )
+        FROM Supplier s
+        JOIN s.orders o ON 
+        ORDER BY o.orderDate DESC
+        """, nativeQuery = false)
+    List<SupplierOrderDTO> findSupplierOrder();
 }
--- backend/src/main/java/com/sandbox/sandman/backend/services/SupplierOrderService.java
+++ backend/src/main/java/com/sandbox/sandman/backend/services/SupplierOrderService.java
@@ -2,18 +2,22 @@ package com.sandbox.sandman.backend.services;
 
 import com.sandbox.sandman.backend.model.dto.SupplierOrderDTO;
 import com.sandbox.sandman.backend.repositories.SupplierOrderRepository;
+import com.sandbox.sandman.backend.repositories.SupplierRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class SupplierOrderService {
 @Autowired
-    private SupplierOrderRepository supplierOrderRepository;
+    private SupplierRepository supplierRepository;
 
     public List<SupplierOrderDTO> getAllSupplierOrders() {
-        return supplierOrderRepository.findAllSupplierOrders();
+        log.info("Process inquiry");
+        return supplierRepository.findSupplierOrder();
     }
 
 }
